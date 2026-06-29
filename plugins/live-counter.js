a08c0cc2fe11334fc8b4cc19e68b23be744e5551
Codemaster <requestse@gmail.com>
2026-06-29 16:04:04 +0300

    Add files via upload

--- plugins/live-counter.js
+++ plugins/live-counter.js
@@ -1,17 +1,24 @@
 NS.liveCounter = ({
     element = "",
     max = 100,
-    counter = "",
+    counterElement = "",
+    remainingElement = "",
+    showCounter = false,
+    showRemaining = false,
     onLimit
 }) => {
     if (typeof onLimit !== "function") return console.log("onLimit arg must be a type of function.");
-    if (!element || !counter || !Number.isInteger(max)) return console.log("Please provide all arguments. Make sure max is a number.");
+    if (!element || !Number.isInteger(max)) return console.log("Please provide all arguments. Make sure max is a number.");
     const foundElement = document.querySelector(element);
-    const foundCounter = document.querySelector(counter);
+    let foundCounter = "";
+    let foundRemaining = "";
+    if (showCounter) foundCounter = document.querySelector(counterElement);
+    if (showRemaining) foundRemaining = document.querySelector(remainingElement);
 
     foundElement.addEventListener("input", function () {
         const length = foundElement.value.length;
-        foundCounter.textContent = `${length}/${max}`;
+        if (showCounter && foundCounter) foundCounter.textContent = `${length}/${max}`;
+        if (showRemaining && foundRemaining) foundRemaining.textContent = max - length;
     });
 
     foundElement.addEventListener('paste', function (e) {
@@ -23,7 +30,8 @@ NS.liveCounter = ({
         if (newLength > max) {
             e.preventDefault();
             foundElement.value += pasted.slice(0, remain);
-            foundCounter.textContent = `${length}/${max}`;
+            if (showCounter && foundCounter) foundCounter.textContent = `${length}/${max}`;
+            if (showRemaining && foundRemaining) foundRemaining.textContent = max - lengt;
 
             onLimit();
         }
