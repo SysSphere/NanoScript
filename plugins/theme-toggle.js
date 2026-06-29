be3300c26ddd0ef41eb22e086121b87223f443e9
Codemaster <requestse@gmail.com>
2026-06-29 10:24:12 +0300

    Add files via upload

---
+++ plugins/theme-toggle.js
@@ -0,0 +1,43 @@
+NS.theme = ({
+    key = "",
+    value = "",
+    theme = "",
+    defaultValue = "light"
+} = {}) => {
+    if (!key || !value || !theme) return console.log("You must provide all args.");
+    const keyValue = localStorage.getItem(key) || defaultValue;
+    if (value === keyValue) document.body.classList.add(theme);
+}
+
+NS.themeToggle = ({
+    key = "",
+    firstValue = "",
+    secondValue = "",
+    theme = "",
+    defaultValue = "light"
+}) => {
+    if (!key || !theme || !firstValue || !secondValue) return console.log("You must provide all args.");
+    const keyValue = localStorage.getItem(key) || defaultValue;
+    document.body.classList.toggle(theme);
+
+    if (document.body.classList.contains(theme)) {
+        localStorage.setItem(key, secondValue);
+    } else {
+        localStorage.setItem(key, firstValue);
+    }
+}
+
+/* Want an example? NS.theme({
+            key: "my-theme",
+            value: "dark",
+            theme: "dark",
+        });
+
+        document.getElementById("theme-toggle").addEventListener("click", function () {
+            NS.themeToggle({
+                firstValue: "light",
+                secondValue: "dark",
+                theme: "dark",
+                key: "my-theme"
+            });
+}); */
