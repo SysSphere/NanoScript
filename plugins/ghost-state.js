b77b06c2486e0f1a0503e499724af2bcf744b7c4
Codemaster <requestse@gmail.com>
2026-07-02 19:33:24 +0300

    Add files via upload

--- plugins/ghost-state.js
+++ plugins/ghost-state.js
@@ -1,22 +1,27 @@
 NS.getGhostState = () => {
     const currentSaves = JSON.parse(localStorage.getItem("ns-current-saves")) || [];
     currentSaves.forEach(save => {
-        document.querySelector(save.selector).value = save.value;
-    })
+        console.log(save);
+        if (document.querySelector(save.selector)) document.querySelector(save.selector)[save.type] = save.value;
+    });
+
+    return currentSaves;
 }
 
-NS.ghostState = (selector) => {
+NS.ghostState = (selector, type = "value") => {
     if (!selector) return console.error("You must provide a selector.");
     const element = document.querySelector(selector);
+    type = type === "text" ? "textContent" : type === "html" ? "innerHTML" : "value";
 
     element.addEventListener("input", function () {
         const currentSaves = JSON.parse(localStorage.getItem("ns-current-saves")) || [];
         const alreadyExists = currentSaves.find(save => save.selector === selector);
 
-        if (alreadyExists) currentSaves[currentSaves.indexOf(alreadyExists)].value = element.value;
+        if (alreadyExists) currentSaves[currentSaves.indexOf(alreadyExists)].value = element[type];
         else currentSaves.push({
             selector: selector,
-            value: element.value
+            type: type,
+            value: element[type]
         });
 
         localStorage.setItem("ns-current-saves", JSON.stringify(currentSaves));
@@ -28,7 +33,7 @@ NS.clearGhostState = (selector, onEnd) => {
 
     let currentSaves = JSON.parse(localStorage.getItem("ns-current-saves")) || [];
     currentSaves = currentSaves.filter(save => save.selector !== selector);
-  
+
     localStorage.setItem("ns-current-saves", JSON.stringify(currentSaves));
     if (typeof onEnd === "function") onEnd();
 }
--- plugins/input-validation.js
+++ plugins/input-validation.js
@@ -1,13 +1,14 @@
-NS.validate = (selector, options = {}) => {
+NS.validate = (selector, type = "value", options = {}) => {
     if (!selector) return console.error("You must pass a selector.");
+    type = type === "text" ? "textContent" : "value"
 
     const foundElement = document.querySelector(selector);
-    if (options?.required && !foundElement.value) return false;
-    if (options?.enum && !options?.enum.includes(foundElement.value)) return false;
+    if (options?.required && !foundElement[type]) return false;
+    if (options?.enum && !options?.enum.includes(foundElement[type])) return false;
     if (foundElement.type === "number") {
-        if (!Number.isInteger(Number(foundElement.value))) return false;
-        if (options?.max !== undefined && foundElement.value > options?.max) return false;
-        if (options?.min !== undefined && foundElement.value < options?.min) return false;
+        if (!Number.isInteger(Number(foundElement[type]))) return false;
+        if (options?.max !== undefined && foundElement[type] > options?.max) return false;
+        if (options?.min !== undefined && foundElement[type] < options?.min) return false;
     }
 
     return true;
